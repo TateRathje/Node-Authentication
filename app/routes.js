1a30ea399c520e990671d0f85afb6a4e4810e3d3
Tate Rathje <taterathje@gmail.com>
2014-08-07 01:42:22 -0700

    Made minor changes to server.js and routes.js, built out authentication veiws.

--- app/routes.js
+++ app/routes.js
@@ -8,7 +8,7 @@ module.exports = function(app, passport) {
 		// LOGIN ====================================
 		// ==========================================
 		// show the login form
-		app.get('./login', function(req, res) {
+		app.get('/login', function(req, res) {
 			res.render('login.ejs', { message: req.flash('loginMessage') });	
 		});
 
@@ -35,5 +35,20 @@ module.exports = function(app, passport) {
 
 		// LOGOUT =====================================
 		// ============================================
-		
+		app.get('/logout', function(req, res) {
+				req.logout();
+				res.redirect('/');
+		});
+};
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+	// is user is authenticated in the session, carry on
+	if (req.isAuthenticated())
+			return next();
+
+	// if they aren't redirect them to the home page
+	res.redirect('/');
+	
 }
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // set up ===================================
 // get all the tools we need
 var express  = require('express');
-var app      = require();
+var app      = express();
 var port     = process.env.PORT || 8080;
 var mongoose = require('mongoose');
 var passport = require('passport');
