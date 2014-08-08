3f9b9cd74b8a62218abf856f57a4ec034d319f7a
Tate Rathje <taterathje@gmail.com>
2014-08-08 03:26:17 -0700

    Finished building out local strategy, added profile page, and updated routes.

--- config/passport.js
+++ config/passport.js
@@ -75,4 +75,35 @@ module.exports = function(passport) {
 
 	}));
 
+  // LOCAL LOGIN ==================================
+  // ==============================================
+  // we are using named strategies since we have one for login and one for signup
+  // by default, if there was no name, it would just be called 'local'
+
+  passport.use('local-login', new LocalStrategy({
+  	usernameField : 'email',
+  	passwordField : 'password',
+  	passReqToCallback : true 
+  },
+  function(req, email, password, done) { // callback with email and password from our form
+
+  	User.findOne({ 'local.email' : email }, function(err, user) {
+  		// if there are any errors, return the error before anything else
+  		if (err)
+  			return done(err);
+
+  		// if no user is found, return the message
+  		if (!user)
+  			return done(null, false, req.flash('loginMessage', 'No user found.'));
+
+  		// if the user is found but the password is wrong
+  		if (!user.validPassword(password))
+  			return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
+
+  		// all is well, return successful user
+  		return done(null, user);
+  	});
+  
+  }));
+
 };
